a1abd510a49591803207b06eb1704d3176648e31
sdnxshu <contact@sudhanshuneravati.com>
2026-02-12 08:57:44 +0530

    feat(auth): add sign-in, sign-up, and forgot-password

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,70 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 
-import { db } from "@/lib/db"; // your drizzle instance
+import { account, session, user, verification } from "@/db/schema";
+import { db } from "@/lib/db";
+import { sendEmail } from "@/utils/send-email";
 
 export const auth = betterAuth({
+
     database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
+        provider: "pg",
+        schema: {
+            user,
+            session,
+            account,
+            verification,
+        }
     }),
+
+    emailAndPassword: {
+
+        enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: true
+
+    },
+
+    emailVerification: {
+
+        autoSignInAfterVerification: false,
+
+        sendOnSignUp: true,
+        sendOnSignIn: true,
+
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            await sendEmail({
+                to: user.email,
+                subject: "Verify your email address",
+                text: `Click the link to verify your email: ${url}`,
+            });
+        },
+
+        onEmailVerification: async (user) => {
+            await sendEmail({
+                to: user.email,
+                subject: "Email verified",
+                text: `Your email has been successfully verified.`,
+            });
+        }
+
+    },
+
+    socialProviders: {
+
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+
+    },
+
+    plugins: [nextCookies()]
+
 });
